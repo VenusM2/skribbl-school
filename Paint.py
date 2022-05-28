eada34a8b576960964d9ff96fdf191fc73d30ef0
VenusM2 <zivrada111@outlook.com>
2022-05-28 09:52:52 +0300

    changes

--- Paint.py
+++ Paint.py
@@ -9,9 +9,13 @@ text.penup()
 randomword.penup()
 text.goto(100, 60)
 text.right(90)
+turtle.speed(0)
 turtle.penup()
-turtle.goto(98,75)
+turtle.goto(98, 75)
 turtle.pendown()
+turtle.hideturtle()
+text.hideturtle()
+randomword.hideturtle()
 
 
 #מצייר לוח
@@ -21,7 +25,7 @@ for c in range(2):
     turtle.forward(150)
     turtle.right(90)
 turtle.pendown()
-turtle.goto(98,-75)
+turtle.goto(98, -75)
 turtle.right(90)
 for c in range(2):
     turtle.right(90)
@@ -29,7 +33,7 @@ for c in range(2):
     turtle.right(90)
     turtle.forward(300)
 turtle.penup()
-turtle.goto(-150,225)
+turtle.goto(-150, 225)
 turtle.right(180)
 turtle.pendown()
 for c in range(2):
@@ -50,7 +54,6 @@ with open(".random_words.txt") as file:
 
 
 # setting players and screen
-screen = turtle.Screen()
 player = turtle.Turtle()
 player.speed(0)
 # setting clear button
@@ -58,13 +61,13 @@ clearbtn = turtle.Turtle()
 clearbtn.speed(0)
 clearbtn.penup()
 clearbtn.shape("square")
-clearbtn.goto(0,-300)
+clearbtn.goto(0, -300)
 # setting eraser button
 eraser = turtle.Turtle()
 eraser.speed(0)
 eraser.penup()
 eraser.shape("square")
-eraser.goto(160,-300)
+eraser.goto(160, -300)
 
 #הגדרת 4 ריבועים TURTLE ל4 צבעים
 Gra = turtle.Turtle()
@@ -73,7 +76,7 @@ Gra.shape("circle")
 Gra.color("gray")
 Gra.shapesize(2)
 Gra.penup()
-Gra.goto(-560,-240 )
+Gra.goto(-560, -240)
 
 Br = turtle.Turtle()
 Br.speed(0)
@@ -81,7 +84,7 @@ Br.shape("circle")
 Br.color("brown")
 Br.shapesize(2)
 Br.penup()
-Br.goto(-400,-240 )
+Br.goto(-400, -240)
 
 G = turtle.Turtle()
 G.speed(0)
@@ -89,7 +92,7 @@ G.shape("circle")
 G.color("green")
 G.shapesize(2)
 G.penup()
-G.goto(-240,-240)
+G.goto(-240, -240)
 
 
 R = turtle.Turtle()
@@ -98,7 +101,7 @@ R.shape("circle")
 R.color("red")
 R.shapesize(2)
 R.penup()
-R.goto(-80,-240)
+R.goto(-80, -240)
 
 
 Y = turtle.Turtle()
@@ -107,7 +110,7 @@ Y.shape("circle")
 Y.color("yellow")
 Y.shapesize(2)
 Y.penup()
-Y.goto(80,-240)
+Y.goto(80, -240)
 
 Bl = turtle.Turtle()
 Bl.speed(0)
@@ -115,7 +118,7 @@ Bl.shape("circle")
 Bl.color("blue")
 Bl.shapesize(2)
 Bl.penup()
-Bl.goto(240,-240 )
+Bl.goto(240, -240)
 
 Pi = turtle.Turtle()
 Pi.speed(0)
@@ -123,7 +126,7 @@ Pi.shape("circle")
 Pi.color("pink")
 Pi.shapesize(2)
 Pi.penup()
-Pi.goto(400,-240)
+Pi.goto(400, -240)
 
 Pu = turtle.Turtle()
 Pu.speed(0)
@@ -131,7 +134,7 @@ Pu.shape("circle")
 Pu.color("purple")
 Pu.shapesize(2)
 Pu.penup()
-Pu.goto(560,-240)
+Pu.goto(560, -240)
 
 Bla = turtle.Turtle()
 Bla.speed(0)
@@ -139,7 +142,7 @@ Bla.shape("circle")
 Bla.color("black")
 Bla.shapesize(2)
 Bla.penup()
-Bla.goto(720,-240)
+Bla.goto(720, -240)
 
 psize2 = turtle.Turtle()
 psize5 = turtle.Turtle()
@@ -162,48 +165,85 @@ psize5.penup()
 psize10.penup()
 
 # fill color
-def fill(x,y):
+
+
+def fill(x, y):
     player.fillcolor()
 
 # setting colors
 # methods for color buttons
-def ColorGreen(x,y):
+
+
+def ColorGreen(x, y):
     player.color("green")
-def ColorRed(x,y):
+
+
+def ColorRed(x, y):
     player.color("red")
-def ColorYellow(x,y):
+
+
+def ColorYellow(x, y):
     player.color("yellow")
-def ColorBlue(x,y):
+
+
+def ColorBlue(x, y):
     player.color("blue")
-def ColorPink(x,y):
+
+
+def ColorPink(x, y):
     player.color("pink")
-def ColorPurple(x,y):
+
+
+def ColorPurple(x, y):
     player.color("purple")
-def ColorBrown(x,y):
+
+
+def ColorBrown(x, y):
     player.color("brown")
-def ColorGray(x,y):
+
+
+def ColorGray(x, y):
     player.color("gray")
-def ColorBlack(x,y):
+
+
+def ColorBlack(x, y):
     player.color("black")
-def eraserWhite(x,y,):
+
+
+def eraserWhite(x, y,):
     player.pencolor("white")
 
+
 def goto(x, y):
+    player.pendown()
     player.ondrag(None)
     player.goto(x, y)
     player.ondrag(goto)
-def pensize2(x,y):
+
+
+def pensize2(x, y):
     player.pensize(2)
-def pensize5(x,y):
+
+
+def pensize5(x, y):
     player.pensize(5)
-def pensize10(x,y):
+
+
+def pensize10(x, y):
     player.pensize(10)
-def teleportto(x,y):
-    player.penup()
-    player.goto(x,y)
-    player.pendown()
-def clearpaint(x,y):
+
+
+def teleportto(x, y):
+    if x > -353 and x < 98 and y < 225 and y > -73:
+        player.penup()
+        player.goto(x, y)
+        player.pendown()
+
+
+def clearpaint(x, y):
     player.clear()
+
+
 def painter():
     # buttons location
     psize2.goto(-200, -300)
@@ -211,19 +251,18 @@ def painter():
     psize10.goto(-300, -300)
 
     # basic UI setup
-    clearbtn.write("clear" , align='center', move=True)
-    eraser.write("eraser" , align='center', move=True)
-    psize2.write("2" , align='center', move=True)
-    psize5.write("5" , align='center', move=True)
-    psize10.write("10" , align='center', move=True)
+    clearbtn.write("clear", align='center', move=True)
+    eraser.write("eraser", align='center', move=True)
+    psize2.write("2", align='center', move=True)
+    psize5.write("5", align='center', move=True)
+    psize10.write("10", align='center', move=True)
 
     # going a bit below the text so the text can be shown
-    clearbtn.goto(0,-310)
-    eraser.goto(160,-310)
+    clearbtn.goto(0, -310)
+    eraser.goto(160, -310)
     psize2.goto(-200, -315)
     psize5.goto(-250, -315)
     psize10.goto(-300, -315)
-
 
     # on click methods
     clearbtn.onclick(clearpaint, btn=3)
@@ -240,9 +279,8 @@ def painter():
     psize5.onclick(pensize5, btn=3)
     psize10.onclick(pensize10, btn=3)
     screen.onclick(teleportto)
-    player.onclick(fill,btn=3)
+    player.onclick(fill, btn=3)
     player.ondrag(goto)
-
 
 
 painter()
